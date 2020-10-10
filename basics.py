01fcdd1d3d92a6421a65e987a5b073e4b533d4dd
P LAKSHMI <69768243+LakshmiPadmanabhan@users.noreply.github.com>
2020-10-10 18:50:24 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -1,7 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-g=nx.Graph()
-G=nx.Graph()
+g=nx.Graph() #Create graph1
+G=nx.Graph() #Creatw graph2
+#Build Graph1
+#Add nodes
 g.add_node('1')
 g.add_node('2')
 g.add_node('3')
@@ -10,6 +12,7 @@ g.add_node('5')
 g.add_node('6')
 g.add_node('7')
 g.add_node('8')
+#Add Edges
 g.add_edge('1','2')
 g.add_edge('1','4')
 g.add_edge('3','2')
@@ -22,12 +25,15 @@ g.add_edge('1','5')
 g.add_edge('2','6')
 g.add_edge('7','3')
 g.add_edge('4','8')
+#Build Graph2
+#Add nodes
 G.add_node('a')
 G.add_node('b')
 G.add_node('c')
 G.add_node('d')
 G.add_node('g')
 G.add_node('h')
+#Add Edges
 G.add_edge('a','g')
 G.add_edge('a','h')
 G.add_edge('a','i')
@@ -44,9 +50,12 @@ nx.draw(g,with_labels=1)
 plt.show()
 nx.draw(G,with_labels=1)
 plt.show()
+#Write Edge Information of 2 graphs into 2 different files
 nx.write_edgelist(g,'edgeList1.txt');
 nx.write_edgelist(G,'edgeList2.txt');
+#Check whether the Graphs are Eulerian
 print('Euler Graphs:')
 print('g: ',nx.is_eulerian(g))
 print('G: ',nx.is_eulerian(G))
+#Check whether the graphs are isomorphic
 print('Isomorphic:',nx.is_isomorphic(g,G))
